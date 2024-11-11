f076d63fdd07c8021e6b73e3fdb889fc3e71928d
AOjeda06 <andres.ojeda@iesnervion.es>
2024-11-11 10:59:03 +0100

    11/11/2024

--- Tema3/src/english/Ej2.java
+++ Tema3/src/english/Ej2.java
@@ -1,10 +1,45 @@
 package english;
 
+import java.util.Scanner;
+
 public class Ej2 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Create scanner
+		Scanner scanner = new Scanner(System.in);
 
+		// Variable declaration
+		int v;
+		int i;
+		int length;
+		int sequence[];
+
+		// Ask for initial value
+		System.out.print("Introduce intial value (V): ");
+		v = scanner.nextInt();
+
+		// Ask for the difference between each number
+		System.out.print("Introduce the sum (I): ");
+		i = scanner.nextInt();
+
+		// Ask for the length of the array
+		System.out.print("Introduce the leght of the array: ");
+		length = scanner.nextInt();
+
+		// Define array's length
+		sequence = new int[length];
+
+		// Fill the array
+		for (int j = 0; j < length; j++) {
+			sequence[j] = v + (i * j);
+		}
+
+		// Show array
+		System.out.println("Result:");
+		for (int value : sequence) {
+			System.out.print(value + " ");
+		}
+
+		// Close scanner
+		scanner.close();
 	}
-
 }
