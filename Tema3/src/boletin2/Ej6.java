ea9d9d0e6aa1facfd5984cbfbf48860e9b68ce15
AOjeda06 <andres.ojeda@iesnervion.es>
2024-11-12 08:31:45 +0100

    12/11/2024

--- Tema3/src/boletin2/Ej6.java
+++ Tema3/src/boletin2/Ej6.java
@@ -8,29 +8,30 @@ public class Ej6 {
 	public static void main(String[] args) {
 		// Creamos el random
 		Random rand = new Random();
+
 		// Variables
 		int apuesta[] = new int[6];
 		int random;
 		int ganador[] = { 1, 2, 3, 4, 5, 6 };
 		int aciertos = 0;
+
 		// Rellenar el array de apuesta con números aleatorios entre 1 y 49
 		for (int i = 0; i < apuesta.length; i++) {
-			random = rand.nextInt(49) + 1;
-			// Generar número aleatorio entre 1 y 49
+			random = rand.nextInt(49) + 1; // Generar número aleatorio entre 1 y 49
 			apuesta[i] = random;
 		}
+
 		// Ordenar el array de ganador
 		Arrays.sort(ganador);
-		// Comparar los números de apuesta con los del array ganador
+
+		// Comparar los números de apuesta con los del array ganador utilizando
+		// binarySearch
 		for (int j = 0; j < apuesta.length; j++) {
-			for (int k = 0; k < ganador.length; k++) {
-				if (apuesta[j] == ganador[k]) {
-					aciertos++;
-					break;
-					// Salir del bucle interno una vez que se encuentra una coincidencia
-				}
+			if (Arrays.binarySearch(ganador, apuesta[j]) >= 0) {
+				aciertos++;
 			}
 		}
+
 		// Imprimir los arrays y el número de aciertos
 		System.out.println("Números ganadores: " + Arrays.toString(ganador));
 		System.out.println("Tu apuesta: " + Arrays.toString(apuesta));
