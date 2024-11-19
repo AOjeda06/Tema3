f27145a159da8624b376fed68109ff95931e40ae
AOjeda06 <andres.ojeda@iesnervion.es>
2024-11-19 14:40:52 +0100

    19/11/2024

--- Tema3/src/boletin4/Ej10.java
+++ Tema3/src/boletin4/Ej10.java
@@ -1,10 +1,64 @@
 package boletin4;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Ej10 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	// Función que verifica si la matriz es mágica
+	public static boolean esMagica(int[][] t) {
+		int sumaFila, sumaCol;
 
+		// Calcular la suma de la primera fila para comparar
+		int sumaReferencia = 0;
+		for (int j = 0; j < t.length; j++) {
+			sumaReferencia += t[0][j];
+		}
+
+		// Verificar la suma de todas las filas
+		for (int i = 1; i < t.length; i++) {
+			sumaFila = 0;
+			for (int j = 0; j < t.length; j++) {
+				sumaFila += t[i][j];
+			}
+			if (sumaFila != sumaReferencia) {
+				return false;
+			}
+		}
+
+		// Verificar la suma de todas las columnas
+		for (int j = 0; j < t.length; j++) {
+			sumaCol = 0;
+			for (int i = 0; i < t.length; i++) {
+				sumaCol += t[i][j];
+			}
+			if (sumaCol != sumaReferencia) {
+				return false;
+			}
+		}
+
+		return true; // Es mágica
 	}
 
+	public static void main(String[] args) {
+		// Variables
+		Random rand = new Random();
+		int[][] t = new int[3][3];
+
+		// Rellena el array con números aleatorios entre 1 y 9
+		for (int i = 0; i < t.length; i++) {
+			for (int j = 0; j < t[i].length; j++) {
+				t[i][j] = rand.nextInt(1, 10);
+			}
+		}
+
+		// Imprime la matriz original
+		System.out.println("Matriz original:");
+		for (int[] fila : t) {
+			System.out.println(Arrays.toString(fila));
+		}
+
+		// Verifica si la matriz es mágica
+		System.out.println("¿La tabla es mágica?: " + esMagica(t));
+	}
 }
