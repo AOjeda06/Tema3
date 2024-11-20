ea3c7ff43b5bdb9bf49eff66d8d5a65a042da877
AOjeda06 <andres.ojeda@iesnervion.es>
2024-11-20 10:11:33 +0100

    20/11/2024

--- Tema3/src/aceptaelreto/BarJavier.java
+++ Tema3/src/aceptaelreto/BarJavier.java
@@ -1,10 +1,12 @@
 package aceptaelreto;
 
+import java.util.Scanner;
+
 public class BarJavier {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
 
+		scanner.close();
 	}
-
 }
